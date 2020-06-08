e9eb00494c5062e65deb7a7daa44a8805aaa1baf
Daniel Koch <daniel.ukonhattu@gmail.com>
2020-06-08 14:50:07 +0300

    Heatmap logging start

---
+++ Assets/Scripts/Heatmap/DataPoint.cs
@@ -0,0 +1,22 @@
+﻿
+/// <summary>
+/// One Point of data for heatmap.
+/// </summary>
+public class DataPoint
+{
+    public int X { set; get; }
+    public int Y { set; get; }
+    public float Size { set; get; }
+
+    public DataPoint(int x, int y, float size)
+    {
+        this.X = x;
+        this.Y = y;
+        this.Size = size;
+    }
+
+    override public string ToString()
+    {
+        return "X: " + this.X + " Y: " + this.Y + " Size: " + this.Size;
+    }
+}
---
+++ Assets/Scripts/Heatmap/HeatmapLogger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Keeps track of heatmap data.
+/// </summary>
+public class HeatmapLogger
+{
+    public List<DataPoint> DataArray { get; }
+
+    public HeatmapLogger(int sizeX, int sizeY)
+    {
+        this.DataArray = new List<DataPoint>();
+    }
+
+    public void AddPoint(DataPoint point)
+    {
+        this.DataArray.Add(point);
+    }
+}
