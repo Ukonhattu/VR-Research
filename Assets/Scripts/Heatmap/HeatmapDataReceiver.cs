6c3b43842d752d1021a0a07e761b6dc1f0407f9d
Daniel Koch <daniel.ukonhattu@gmail.com>
2020-06-09 13:50:48 +0300

    Receive heatmao data

---
+++ Assets/Scripts/Heatmap/HeatmapDataReceiver.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatmapDataReceiver : MonoBehaviour
+{
+    private HeatmapLogger logger;
+
+    private void Start()
+    {
+        this.logger = new HeatmapLogger();
+    }
+
+    public void ReceiveDataPoint(DataPoint dataPoint)
+    {
+        this.logger.AddPoint(dataPoint);
+        Debug.Log("Received DataPoint: " + dataPoint);
+    }
+
+    public void ReceivePosition(Vector3 position, float size)
+    {
+        DataPoint dataPoint = new DataPoint(position, size);
+        this.ReceiveDataPoint(dataPoint);
+    }
+
+    public void SaveData()
+    {
+        this.logger.SaveData();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.SaveData();
+        }
+    }
+}
