be3e1e75afb977998ce87dce0a51ebcf4da27c47
Daniel Koch <daniel.ukonhattu@gmail.com>
2020-06-16 16:53:54 +0300

    Text plane with text mesh pro and changable font size

---
+++ Assets/Scripts/Panel/ChangeFontSize.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Leap.Unity.Interaction;
+
+public class ChangeFontSize : MonoBehaviour
+{
+
+    public TextMeshProUGUI text;
+    public InteractionSlider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Change()
+    {
+        this.text.fontSize = slider.HorizontalSliderValue;
+    }
+}
