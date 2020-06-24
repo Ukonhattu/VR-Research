f760be9e43496bd9c351ba85f7fb24888115f207
Daniel Koch <daniel.ukonhattu@gmail.com>
2020-06-24 12:58:17 +0300

    updated NetworkObject

--- Assets/Scripts/Network/NetworkObject.cs
+++ Assets/Scripts/Network/NetworkObject.cs
@@ -1,9 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System;
 
 public class NetworkObject
 {
     public string ObjectName { get; set; }
     public string Object { get; set; }
+
+    // Should this be illegal?
+    public NetworkObject(string json)
+    {
+        NetworkObject newObject = JsonUtility.FromJson<NetworkObject>(json);
+        this.ObjectName = newObject.ObjectName;
+        this.Object = newObject.Object;
+    }
+
+    public NetworkObject()
+    {
+        this.ObjectName = null;
+        this.Object = null;
+    }
+
+    public NetworkObject(string objectName, string objectjson)
+    {
+        this.ObjectName = ObjectName;
+        this.ObjectName = objectjson;
+    }
+
+    public object GetObject()
+    {
+        try
+        {
+            Type type = Type.GetType(ObjectName);
+            return JsonUtility.FromJson(this.Object, type);
+        }
+        catch (Exception e)
+        {
+            throw new Exception("Cannot convert Object to " + ObjectName);
+        }
+    }
 }
