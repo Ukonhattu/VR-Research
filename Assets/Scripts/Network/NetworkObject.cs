9bc0158ead9c8381b165ee52c083c7d9c892107a
Daniel Koch <daniel.ukonhattu@gmail.com>
2020-06-24 12:04:21 +0300

    Network object

---
+++ Assets/Scripts/Network/NetworkObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkObject
+{
+    public string ObjectName { get; set; }
+    public string Object { get; set; }
+}
