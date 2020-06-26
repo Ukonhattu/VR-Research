123eaf4ae2f63838587f366c2b11392986272fe9
Daniel Koch <daniel.ukonhattu@gmail.com>
2020-06-26 13:59:49 +0300

    HttpSender command line tool

--- Python/HttpSender.py
+++ Python/HttpSender.py
@@ -1,8 +1,39 @@
 import requests
+import sys
+import getopt
 
-url = 'http://localhost:4444'
-myobj = "{\"ObjectName\": \"ImageData\", \"Object\":\"{\\\"ImagePath\\\": \\\"somevalue\\\", \\\"ViewSeconds\\\": 4.0, \\\"Id\\\": 1}\"}"
 
-x = requests.post(url, data = myobj)
+def main(argv):
 
-print(x.text)
+    objectName = "SomeName"
+    imagePath = "Some//Path//"
+    viewSeconds = 4.0
+    oid = 1
+
+    try:
+        opts, args = getopt.getopt(argv,"ho:p:v:i:",["objectName=","imagePath=", "viewSeconds=", "id="])
+    except getopt.GetoptError:
+        print ('HttpSender.py -o <objectName> -p <imagePath> -v <viewSeconds> -i <id>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('HttpSender.py -o <objectName> -p <imagePath> -v <viewSeconds> -i <id>')
+            sys.exit()
+        elif opt in ("-o", "--objectName"):
+            objectName = arg
+        elif opt in ("-p", "--imagePath"):
+            imagePath = arg
+        elif opt in ("-v", "--viewSeconds"):
+            viewSeconds = arg
+        elif opt in ("-i", "--id"):
+            oid = arg
+    
+    url = 'http://localhost:4444'
+    data = '{{ "ObjectName": "{0}", "Object": "{{ "ImagePath": "{1}", "ViewSeconds": {2}, "Id": {3} }}" }}'.format(objectName, imagePath, viewSeconds, oid)
+    print(data)
+    x = requests.post(url, data = data)
+
+    print(x.text)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
